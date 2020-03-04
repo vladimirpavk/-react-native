464d042588c23c8d2472fe6612d327f4971326f6
Vladimir Pavković <vladimirpavk@gmail.com>
2020-03-04 09:18:18 +0100

    +

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 interface MyProps{}
 interface MyState{
-  name: string
+  name: string,
+  anyText: string
 }
 
 class App extends Component<MyProps, MyState>{
@@ -12,35 +13,48 @@ class App extends Component<MyProps, MyState>{
   constructor(props){
     super(props);
     this.state={
-      name : ''
+      name : '',
+      anyText: ''
     };
   }   
 
+  textChanged = (text, event)=>{
+    //console.log(text, event);
+    this.setState({
+      anyText: text
+    })
+  }
+
   render(){
     return(
-      <View style={styles.container}>
-        <Text style={styles.text}>Open up App.tsx to start on your app!</Text>
-        <TextInput
-          style={styles.text}
-          onChangeText={(text)=>this.setState({name: text})}       
-          value={this.state.name}   
-        ></TextInput>
+      <View style={{flex:1, flexDirection: 'column'}}>
+        <View style={styles.stil1}>
+          <TextInput
+            style={{flex:1}}
+            placeholder="Type any text in da box"
+            onChangeText={(text)=>this.textChanged(text, event)}
+            value={this.state.anyText}
+          ></TextInput>
+        </View>
+        <View style={styles.stil2}></View>
+        <View style={styles.stil3}></View>
       </View>
     );
   } 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center'
+  stil1: {
+    flex:1,
+    backgroundColor: 'powderblue'
   },
-  text: {
-    color: 'black',
-    border: '1px solid red',
-    backgroundColor: 'green'
+  stil2: {
+    flex:2,
+    backgroundColor: 'skyblue'
+  },
+  stil3: {
+    flex: 3,
+    backgroundColor: 'steelblue'
   }
 });
 
